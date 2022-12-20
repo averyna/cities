3f6f2ca08ef6ff82a8aaef5dbd330ff415b42429
Olga Averyna <olga.averyna@helmes.com>
2022-12-20 08:34:50 +0100

    disable csv load in tests

--- src/test/java/com/helmes/cities/CitiesApplicationTest.java
+++ src/test/java/com/helmes/cities/CitiesApplicationTest.java
@@ -1,10 +1,14 @@
 package com.helmes.cities;
 
+import com.helmes.cities.adapter.out.helper.CityCSVLoader;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class CitiesApplicationTest {
+	@MockBean
+	CityCSVLoader cityCSVLoader;
 
 	@Test
 	void contextLoads() {
